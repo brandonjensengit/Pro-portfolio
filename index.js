da313c9a68afa69801a456f17fd32fb0fa8811d2
Brandon Jensen <brandon@brandonjensen.info>
2023-09-05 14:31:02 -0600

    Updated resume and added comments to JS code

--- index.js
+++ index.js
@@ -2,28 +2,32 @@ let isModalOpen = false;
 let contrastToggle = false;
 const scaleFactor = 1 / 20;
 
+// Moves the small shape elements in the background
+
 function moveBackground(event) {
-  const shapes = document.querySelectorAll(".shape")
+  const shapes = document.querySelectorAll(".shape");
   const x = event.clientX * scaleFactor;
   const y = event.clientY * scaleFactor;
-  
+
   for (let i = 0; i < shapes.length; i++) {
     const isOdd = i % 2 !== 0;
     const boolInt = isOdd ? -1 : 1;
-    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`;
   }
 }
 
+// Toggles dark mode on and off
 
 function toggleContrast() {
   contrastToggle = !contrastToggle;
   if (contrastToggle) {
     document.body.classList += " dark-theme";
-  }
-  else {
-    document.body.classList.remove("dark-theme")
+  } else {
+    document.body.classList.remove("dark-theme");
   }
 }
+
+// Overlays for contact modal
 
 function contact(event) {
   event.preventDefault();
@@ -49,6 +53,8 @@ function contact(event) {
       );
     });
 }
+
+// Opens and closes contact modal
 
 function toggleModal() {
   if (isModalOpen) {
