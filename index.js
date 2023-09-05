a93aad708c35c68058c70abd1b1117f0f7956646
Brandon Jensen <brandon@brandonjensen.info>
2023-09-05 12:45:06 -0600

    Added remaining styling, dark mode, animations, and responsive code

---
+++ index.js
@@ -0,0 +1,60 @@
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape")
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+  
+  for (let i = 0; i < shapes.length; i++) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+  }
+}
+
+
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += " dark-theme";
+  }
+  else {
+    document.body.classList.remove("dark-theme")
+  }
+}
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+
+  emailjs
+    .sendForm(
+      "service_12nig3k",
+      "template_cdoe1ma",
+      event.target,
+      "77f83N1J3qnYYloIQ"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable.  Please contact me directly at brandon@brandonjensen.info"
+      );
+    });
+}
+
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = !isModalOpen;
+  document.body.classList += " modal--open";
+}
